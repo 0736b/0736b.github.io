751705ead8da857d50eb7e77d6930a575f255b1a
0736b <thanawat_bumpengpun@cmu.ac.th>
2022-12-09 19:47:53 +0700

    add: docs

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-static"; 
+// was "@sveltejs/adapter-auto"
 
-/** @type {import('@sveltejs/kit').Config} */
+const dev = "production" === "development";
+
+/** @type {import(""@sveltejs/kit").Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+    kit: {
+        adapter: adapter({
+            pages: "docs",
+            assets: "docs",
+			fallback: null,
+			precompress: false,
+			strict: true
+        }),
+        // hydrate the <div id="svelte"> element in src/app.html
+        // target: "#svelte"
+    }
 };
 
 export default config;
