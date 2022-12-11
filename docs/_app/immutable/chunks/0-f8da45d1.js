f6763bfa95c26da9967d247e96bc6ce21cc736e8
0736b <thanawat_bumpengpun@cmu.ac.th>
2022-12-11 14:58:09 +0700

    feat: tailwind css

---
+++ docs/_app/immutable/chunks/0-f8da45d1.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_layout-80922366.js";import{default as t}from"../components/pages/_layout.svelte-af07c81e.js";export{t as component,r as shared};
--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,7 @@ import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 import adapter from '@sveltejs/adapter-static';
 // was "@sveltejs/adapter-auto"
+import preprocess from "svelte-preprocess";
 
 const dev = 'production' === 'development';
 
@@ -21,13 +22,13 @@ const config = {
 		// target: "#svelte"
 	},
 
-	preprocess: [mdsvex(mdsvexConfig)]
-	// preprocess: [
-    //     preprocess({
-    //         postcss: true
-    //     }),
-    //     mdsvex(mdsvexConfig)
-    // ]
+	// preprocess: [mdsvex(mdsvexConfig)]
+	preprocess: [
+        preprocess({
+            postcss: true
+        }),
+        mdsvex(mdsvexConfig)
+    ]
 };
 
 export default config;
