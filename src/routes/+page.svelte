c2bb63b5c5705d1b08ae5871b00a180481b26151
0736b <thanawat_bumpengpun@cmu.ac.th>
2023-01-17 02:28:35 +0700

    remove: bg noise

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,18 @@
 <div class="introduce container mx-auto max-w-3xl mt-10 bg-[#D4D4D4] text-[#2637DE] px-14 py-16">
     <h1 class="uppercase font-rubik text-xl mb-8 tracking-widest font-medium">Who Am I?  ➡</h1>
     <h1 class="uppercase font-rubik font-black text-5xl lg:text-7xl">I'm Thanawat</h1>
-    <h2 class="font-robotomono text-xl py-4">Computer engineering student. <br>programmer by day, wanna be game-hacker by night.</h2>
+    <h2 class="font-robotomono text-xl py-4">A year three computer engineering student at Chiang Mai University.</h2>
     <hr class="my-4 mx-auto h-0.5 border-0 bg-[#2637DE]">
-    <p class="font-robotomono pt-4 text-lg font-light">I'm a computer engineering undergraduate student at Faculty of Engineering, Chiang Mai University. I'm interested in Software/Web Development, UI/UX Design, Machine Learning, Cheat Development and some Reverse Engineering.</p>
+    <p class="font-robotomono pt-4 text-lg font-light">I love developing software that makes life easier. Love the aesthetics of the user interface and great user experience.
+</p>
 </div>
 
 <div class="skill container mx-auto max-w-3xl bg-[#D4D4D4] text-[#161616] px-14 pb-20">
     <h1 class="uppercase font-rubik text-xl mb-8 tracking-widest font-medium text-right">⬅  Skills</h1>
     <h1 class="uppercase font-rubik font-black text-3xl lg:text-6xl text-right mb-4">Technologies and Languages</h1>
-    <h2 class="text-xl py-2"><span class="font-rubik font-bold text-2xl tracking-tighter">Languages </span> - <span class="font-robotomono font-bold text-lg"> C++, Java, Python, JavaScript, SQL, HTML, CSS</span></h2>
-    <h2 class="text-xl py-2"><span class="font-rubik font-bold text-2xl tracking-tighter">Frameworks </span> - <span class="font-robotomono font-bold text-lg"> React, Laravel, Svelte, TailwindCSS, Bootstrap</span></h2>
-    <h2 class="text-xl py-2"><span class="font-rubik font-bold text-2xl tracking-tighter">Tools </span> - <span class="font-robotomono font-bold text-lg"> Git, Docker, VMware, Figma, MS Word, MS PowerPoint, Photoshop, Lightroom</span></h2>
+    <h2 class="text-xl py-2"><span class="font-rubik font-bold text-2xl tracking-tighter">Languages </span> - <span class="font-robotomono font-bold text-lg"> Python, Java, JavaScript, SQL</span></h2>
+    <h2 class="text-xl py-2"><span class="font-rubik font-bold text-2xl tracking-tighter">Frameworks </span> - <span class="font-robotomono font-bold text-lg"> React, Svelte, Django, Flask, Tailwind CSS, Bootstrap</span></h2>
+    <h2 class="text-xl py-2"><span class="font-rubik font-bold text-2xl tracking-tighter">Tools </span> - <span class="font-robotomono font-bold text-lg"> Git, Docker, Figma, Postman, DB Browser</span></h2>
     <h2 class="text-xl"><span class="font-rubik font-bold text-2xl tracking-tighter"><span class="text-sm font-robotomono font-bold">(software reverse engineering.)</span></span> - <span class="font-robotomono font-bold text-sm"> x64dbg, dnSpy, HTTP Debugger, Cheat Engine</span></h2>
 </div>
 
